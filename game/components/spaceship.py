8129653426efa44e3ae81bff369a6d859c0186af
josux1 <micorreoonline6@gmail.com>
2023-06-16 19:55:14 -0600

    Tarea mejorada

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 from game.utils.constants import SPACESHIP
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 # Sprite = Objeto Dibujable
 class Spaceship(Sprite):
@@ -10,13 +11,22 @@ class Spaceship(Sprite):
         self.image = SPACESHIP
         self.image = pygame.transform.scale(self.image, (self.image_width, self.image_height))
         self.rect = self.image.get_rect()
+        self.rect.y = SCREEN_HEIGHT - self.image_height
+        self.rect.x = SCREEN_WIDTH // 2
+        self.speed = 20
         
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
     
     def update(self, keyboard_events):
-        if keyboard_events[pygame.K_LEFT]:
-            self.rect.x += -15
-        if keyboard_events[pygame.K_RIGHT]:
-            self.rect.x += 15
+        if (keyboard_events[pygame.K_LEFT] and self.rect.x >= 0):
+            self.move_left()
+        if (keyboard_events[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH - self.image_width):
+            self.move_right()
+
+    def move_left(self):
+        self.rect.x -= self.speed
+
+    def move_right(self):
+        self.rect.x += self.speed
         
